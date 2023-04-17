198da41237c92a1aaf2466c5a2775c6b792134a9
kellylau <kellylaup@gmail.com>
2023-04-17 00:10:16 -0400

    updated main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "test-ascii.h"
 #include "caesar.h"
 #include "vigenere.h"
-#include "decrypt.h"
+#include "decryption.h"
 
 
 int main()
@@ -11,7 +11,8 @@ std::cout << encryptCaesar("Way to Go!",5) << '\n';
 std::cout << encryptVigenere("Hello, World!", "cake")<< '\n'; 
 std::cout << decryptCaesar("Rovvy, Gybvn!", 10)<< '\n'; 
 std::cout << decryptVigenere("Jevpq, Wyvnd!", "cake")<< '\n'; 
-
+std::cout << solve(encryptCaesar("I love cats and dogs", 5)) << std::endl;    
+std::cout << solve(encryptCaesar("I love cats from the moon and back", 10)) << std::endl;
 
 return 0;
 }
