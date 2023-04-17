f4f580989f2b393e2c8f51c7bd71a10eb7ada387
kellylau <kellylaup@gmail.com>
2023-04-17 00:10:44 -0400

    Updated decrypt

--- decryption.h
+++ decryption.h
@@ -3,3 +3,8 @@
 char shift_back(char, int); 
 std::string decryptCaesar(std::string, int); 
 std::string decryptVigenere(std::string, std::string); 
+double distance_vectors(std::vector<double>, std::vector<double>);
+double letter_frequency(char, std::string);
+std::vector<double> string_frequency(std::string); 
+std::string solve(std::string);
+
